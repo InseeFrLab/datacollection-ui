112d59b42cfddf4dd85d19bc79667843b407cfb3
Laurent Caouissin <laurent.caouissin@agriculture.gouv.fr>
2022-06-20 17:39:59 +0200

    Add id selection for contact (fake auth)

--- src/components/auth/provider/noAuth.js
+++ src/components/auth/provider/noAuth.js
@@ -4,16 +4,22 @@ import {
   InputLabel,
   MenuItem,
   Select,
-} from '@mui/material';
-import React, { useContext, useEffect, useState } from 'react';
-import { AppContext } from '../../../App';
-import { useAPI } from '../../../utils/hooks/api';
+  TextField,
+  Typography,
+} from "@mui/material";
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../../../App";
+import { useAPI } from "../../../utils/hooks/api";
 
 export const NoAuthLogin = ({ setId }) => {
   const { apiUrl, setLoading } = useContext(AppContext);
   const [contacts, setContacts] = useState([]);
-  const [selected, setSelected] = useState('');
+  const [selected, setSelected] = useState("");
   const { getFirstContacts } = useAPI();
+
+  const [idSelected, setIdSelected] = useState("");
+
+  const [chooseType, setChooseType] = useState("");
 
   useEffect(() => {
     const loadContact = async () => {
@@ -24,52 +30,99 @@ export const NoAuthLogin = ({ setId }) => {
           _embedded: { contacts: contactsFetched },
         } = data;
         setContacts(contactsFetched);
-        setLoading(false);
       }
+
+      setLoading(false);
     };
-    if (contacts.length === 0) loadContact();
+    if (contacts.length === 0 && chooseType === "list") loadContact();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [contacts]);
+  }, [contacts, chooseType]);
 
-  const handleChange = event => {
+  const handleChangeChooseType = (event) => {
+    setChooseType(event.target.value);
+  };
+  const handleChangeSelect = (event) => {
     setSelected(event.target.value);
   };
 
-  const chooseUser = id => {
-    const {
-      _links: {
-        self: { href: uriAcess },
-      },
-    } = contacts[id];
-    const retrievedId = uriAcess.split(`${apiUrl}/contacts/`)[1];
-    setId(retrievedId);
+  const handleChangeInput = (event) => {
+    setIdSelected(event.target.value);
+  };
+
+  const chooseUser = (type) => {
+    if (type === "list") {
+      const {
+        _links: {
+          self: { href: uriAcess },
+        },
+      } = contacts[selected];
+      const retrievedId = uriAcess.split(`${apiUrl}/contacts/`)[1];
+      setId(retrievedId);
+    } else if (type === "id") setId(idSelected);
   };
 
   return (
-    <div>
+    <div className="form-no-auth">
+      <Typography variant="h3">Veuillez choisir un contact</Typography>
+      <br />
       <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Contact</InputLabel>
+        <InputLabel id="choose-label">Choix du contact</InputLabel>
         <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          value={selected}
-          label="Age"
-          onChange={handleChange}
+          labelId="choose-label"
+          id="choose-simple-select"
+          value={chooseType}
+          label="Contact"
+          onChange={handleChangeChooseType}
         >
-          {contacts.map(
-            ({ lastName, firstName, function: serviceFunction }, index) => (
-              <MenuItem
-                key={`${index}-item`}
-                value={index}
-              >{`${lastName} ${firstName} - ${serviceFunction}`}</MenuItem>
-            )
-          )}
+          {[
+            { type: "list", label: "Choisir dans une liste" },
+            { type: "id", label: "Avec un identifiant" },
+          ].map(({ type, label }, index) => (
+            <MenuItem key={`${index}-item`} value={type}>
+              {label}
+            </MenuItem>
+          ))}
         </Select>
+        <br />
+        {chooseType === "id" && (
+          <>
+            <TextField
+              id="demo-simple-text"
+              value={idSelected}
+              label="Identifiant"
+              onChange={handleChangeInput}
+            />
+          </>
+        )}
+        {chooseType === "list" && contacts.length > 0 && (
+          <>
+            <InputLabel id="demo-simple-select-label">Contact</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={selected}
+              label="Contact"
+              onChange={handleChangeSelect}
+            >
+              {contacts.map(
+                ({ lastName, firstName, function: serviceFunction }, index) => (
+                  <MenuItem
+                    key={`${index}-item`}
+                    value={index}
+                  >{`${lastName} ${firstName} - ${serviceFunction}`}</MenuItem>
+                )
+              )}
+            </Select>
+          </>
+        )}
       </FormControl>
-      <Button onClick={() => chooseUser(selected)}>
-        Choisir l'utilisateur
-      </Button>
+
+      {chooseType && (selected || idSelected) && (
+        <Button onClick={() => chooseUser(chooseType)}>
+          Choisir l'utilisateur
+        </Button>
+      )}
     </div>
   );
 };
--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -1,40 +1,40 @@
-import { rows, sleep } from '../mock/surveys';
-import { fetcher } from './fetcher';
+import { rows, sleep } from "../mock/surveys";
+import { fetcher } from "./fetcher";
 
-const putRequest = url => body => token =>
-  fetcher(url, token, 'PUT', null, body);
+const putRequest = (url) => (body) => (token) =>
+  fetcher(url, token, "PUT", null, body);
 /*
   const postRequest = (url) => (token) => (body) =>
 	fetcher(url, token, "POST", body);
 const deleteRequest = (url) => (token) => (body) =>
 	fetcher(url, token, "DELETE", body);
 */
-const getRequest = url => params => token =>
-  fetcher(url, token, 'GET', params, null);
+const getRequest = (url) => (params) => (token) =>
+  fetcher(url, token, "GET", params, null);
 
-const mockGetMySurveys = apiUrl => id => async token => {
+const mockGetMySurveys = (apiUrl) => (id) => async (token) => {
   await sleep(2000);
-  return { data: rows, status: 200, statusText: 'ok', error: false };
+  return { data: rows, status: 200, statusText: "ok", error: false };
 };
 
-const getMySurveys = apiUrl => id => async token => {
+const getMySurveys = (apiUrl) => (id) => async (token) => {
   return getRequest(`${apiUrl}/mySurveys/${id}`)(null)(token);
 };
 
-const getContacts = apiUrl => async token => {
-  const params = { page: 0, size: 15 };
+const getContacts = (apiUrl) => async (token) => {
+  const params = { page: 0, size: 50 };
   return getRequest(`${apiUrl}/contacts`)(params)(token);
 };
 
-const getContact = apiUrl => id => async token => {
+const getContact = (apiUrl) => (id) => async (token) => {
   return getRequest(`${apiUrl}/contacts/${id}`)(null)(token);
 };
 
-const getContactAddress = apiUrl => id => async token => {
+const getContactAddress = (apiUrl) => (id) => async (token) => {
   return getRequest(`${apiUrl}/contacts/${id}/address`)(null)(token);
 };
 
-const putAddress = url => body => async token => {
+const putAddress = (url) => (body) => async (token) => {
   return putRequest(url)(body)(token);
 };
 
